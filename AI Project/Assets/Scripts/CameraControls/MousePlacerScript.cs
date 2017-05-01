d509c48e8616fce7254119a399782b1895b9632e
Rygaido <rygaido@gmail.com>
2017-04-30 21:55:12 -0400

    camera controlls

---
+++ AI Project/Assets/Scripts/CameraControls/MousePlacerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePlacerScript : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit rh;
+        Ray r = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(r, out rh))
+        {
+            transform.position = rh.point;
+        }
+	}
+}
